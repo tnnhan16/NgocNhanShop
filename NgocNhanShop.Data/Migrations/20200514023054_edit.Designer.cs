cfb88c547d9b5d146384661b96571c3b089bbb05
tnnhan <tnnhan@ctu.edu.vn>
2020-05-14 09:39:10 +0700

    create identity database

--- NgocNhanShop.Data/Migrations/20200514023054_edit.Designer.cs
+++ NgocNhanShop.Data/Migrations/20200514023054_edit.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NgocNhanShop.EF.Data;
 
 namespace NgocNhanShop.Data.Migrations
 {
     [DbContext(typeof(NgocNhanShopDbContext))]
-    partial class NgocNhanShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200514023054_edit")]
+    partial class edit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
